2db2497e92fcbb063c0b97f6b7171fd6a72f5f74
Drew <drew.m.white51@gmail.com>
2020-12-17 14:00:20 -0600

    Added exmaple 1, employee class and driver.

---
+++ JavaChapter6/src/main/java/EmployeeExample/EmployeeClassDriver_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EmployeeExample;
+
+/**
+ *
+ * @author Drew
+ */
+public class EmployeeClassDriver_1 {
+    public static void main(String[] args) {
+        EmployeeClass_1 firstEmployee = new EmployeeClass_1("Susan", "Meyers", 47899, "Accounting", "VP");
+        EmployeeClass_1 secondEmployee = new EmployeeClass_1("Mark", "Jones", 39119, "IT", "Programmer");
+        EmployeeClass_1 thirdEmployee = new EmployeeClass_1("Joy", "Rogers", 81774, "Manufacturing", "Engineer");
+        System.out.println("Name      /      ID Number      /      Deparment       /        Position      /");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println(firstEmployee);
+        System.out.println(secondEmployee);
+        System.out.println(thirdEmployee);
+    }
+}
